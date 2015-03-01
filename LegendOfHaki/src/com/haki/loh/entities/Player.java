8413735c0ba1fe780dafb9269437fbb9c7ac7a8d
Andre Vicencio M <Andre Vicencio M@WINDOWS-7JU3O1K>
2015-03-01 23:33:52 +1100

    edit play starting point

--- LegendOfHaki/src/com/haki/loh/entities/Player.java
+++ LegendOfHaki/src/com/haki/loh/entities/Player.java
@@ -36,7 +36,7 @@ public class Player extends Entity {
 
 		world = state.getWorld();
 		BodyDef bdef = new BodyDef();
-		bdef.position.set(300 / PPM, 200 / PPM);
+		bdef.position.set(20 / PPM, 200 / PPM);
 		bdef.type = BodyType.DynamicBody;
 		body = world.createBody(bdef);
 		PolygonShape shape = new PolygonShape();
